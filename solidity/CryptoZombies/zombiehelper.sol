b3412b53541730d7b8e6f841f0f2c6b929d00ddb
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-04-29 22:44:40 +0530

    external view functions don't need any gas

--- solidity/CryptoZombies/zombiehelper.sol
+++ solidity/CryptoZombies/zombiehelper.sol
@@ -9,15 +9,18 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
-  // Start here
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
-      require(msg.sender == zombieToOwner[_zombieId]);
-      zombies[_zombieId].name = _newName;
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
   }
 
   function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-      require(msg.sender == zombieToOwner[_zombieId]);
-      zombies[_zombieId].dna = _newDna;
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
+
+  // Create your function here
+  function getZombiesByOwner(address _owner) external view returns (uint[]) {
   }
 
 }
