1bf8d7f9db5cbac440966073dd071be70128fb84
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-04-29 16:23:31 +0530

    ERROR: Called parent contract's PRIVATE method

--- solidity/CryptoZombies/zombiefeeding.sol
+++ solidity/CryptoZombies/zombiefeeding.sol
@@ -4,10 +4,13 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
-  // Start here
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-      require(msg.sender == zombieToOwner[_zombieId]);
-      Zombie storage myZombie = zombies[_zombieId];
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    // start here
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
   }
 
 }
