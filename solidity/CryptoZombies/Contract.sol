d89fc0828fa55491f1e41de2da8a8ad2ae5f5f40
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-04-29 15:55:23 +0530

    Learnt mappings

--- solidity/CryptoZombies/Contract.sol
+++ solidity/CryptoZombies/Contract.sol
@@ -2,8 +2,7 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
-    // declare our event here
-    event newZombie(uint zombieId, string name, uint dna);
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -15,10 +14,13 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    // declare mappings here
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        // and fire it here
-        newZombie(id, _name, _dna);
+        NewZombie(id, _name, _dna);
     } 
 
     function _generateRandomDna(string _str) private view returns (uint) {
