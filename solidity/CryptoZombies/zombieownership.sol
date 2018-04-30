0518717a5ec2a3b73213132a7861b63578c9ba48
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-04-30 22:20:19 +0530

    Multiple inheritance and ERC721

--- solidity/CryptoZombies/zombieownership.sol
+++ solidity/CryptoZombies/zombieownership.sol
@@ -1,8 +1,10 @@
-// Start here
 pragma solidity ^0.4.19;
 
 import "./zombieattack.sol";
+// Import file here
+import "./erc721.sol";
 
-contract ZombieOwnership is ZombieAttack {
+// Declare ERC721 inheritance here
+contract ZombieOwnership is ZombieAttack, ERC721 {
     
 }
