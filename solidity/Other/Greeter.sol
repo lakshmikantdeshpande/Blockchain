2dbb18396a0e605ab59d087dcce6ce33b54c4829
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-05-02 22:37:13 +0530

    Mortal selfdestruct contract

---
+++ solidity/Other/Greeter.sol
@@ -0,0 +1,27 @@
+contract Mortal {
+
+	address mortal;
+
+	function Mortal() {
+		owner = msg.sender;
+	}
+
+	function kill() {
+		if (msg.sender == owner) selfdestruct(owner);
+	}
+
+}
+
+contract Greeter is Mortal {
+	
+	string greeting;
+
+	function Greeter(string _greeting) public {
+		greeting = _greeting;
+	}
+
+	function greet() constant returns (string) {
+		return greeting;
+	}
+
+}
